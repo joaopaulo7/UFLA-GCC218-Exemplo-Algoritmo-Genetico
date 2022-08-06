f9dfeb841b6c7df1bbd9368319dab80a9ae31680
annielu2 <aluizacalixto@gmail.com>
2022-08-06 18:18:02 -0300

    arquivos.py

---
+++ Funcao.py
@@ -0,0 +1,19 @@
+'''
+Trabalho: Exercício sobre Algoritmos Genético da matéria de Inteligência Artificial GCC128
+Alunos: Ana Luiza Faria Calixto - João Paulo Paiva Lima
+
+O trabalho tem como intuito encontrar o valor de x para a função f(x) = x*x - 3x + 4, onde x
+será o valor máximo.
+
+'''
+populacao = []
+mutacao = 0.01
+crossover = 0.7
+geracoes = 5
+x_min = -10
+x_max = 10
+num_individuos = 4
+
+
+
+
